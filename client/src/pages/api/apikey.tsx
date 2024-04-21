b1b6a77ea371f51a293ecf7edf498f2659a96d44
Eddie2111 <asm.tareq.mahmood@g.bracu.ac.bd>
2024-04-21 20:35:49 +0600

    fix:vercel csr fix, ui changes, apikey updated for deployment

--- client/src/pages/api/apikey.tsx
+++ client/src/pages/api/apikey.tsx
@@ -1,3 +1,3 @@
-// const apikey: string = 'https://muzzplayer-service.onrender.com' as string;
-const apikey: string = 'http://localhost:3200' as string;
+const apikey: string = 'https://muzzplayer-service.onrender.com' as string;
+// const apikey: string = 'http://localhost:3200' as string;
 export default apikey;
